480eb2c6b9839e07af486abc3c8c3f62f0487ad2
Hazem-Ben-Abdelhafidh <morbidvisions@yahoo.com>
2022-06-26 18:58:29 +0100

    adding animation to the skills using framer motion

--- src/Components/AppRoutes.jsx
+++ src/Components/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Contact from "./Contact/Contact";
 import Home from "./Home";
+import Projects from "./Porjects/Projects";
 import Skills from "./Skills/Skills";
 
 const AppRoutes = () => {
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="Skills" element={<Skills />} />
       <Route path="Contact" element={<Contact />} />
+      <Route path="Projects" element={<Projects />} />
     </Routes>
   );
 };
