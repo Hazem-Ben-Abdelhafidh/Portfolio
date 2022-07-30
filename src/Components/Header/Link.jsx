44e4d77ed5b13fd4a183e438cc47ba923b79a8f9
Hazem-Ben-Abdelhafidh <morbidvisions@yahoo.com>
2022-07-30 15:40:46 +0100

    Configuring active links

--- src/Components/Header/Link.jsx
+++ src/Components/Header/Link.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
-const Link = ({ path, icon }) => {
+const Link = ({ path, icon, name }) => {
+  const [active, setActive] = useState(false);
   return (
-    <li className=" h-10 w-10  flex justify-center items-center rounded-full">
+    <li className=" h-10 w-10 relative flex cursor-pointer justify-center flex-col items-center rounded-full group">
       <NavLink
         to={path}
-        className=" text-2xl hover:text-blue-500 transition-colors"
+        className={(data) => {
+          setActive(data.isActive);
+          console.log(data.isActive);
+          return data.isActive
+            ? "text-2xl text-blue-500 group-hover:text-blue-500 duration-500"
+            : "text-2xl group-hover:text-blue-500 duration-500";
+        }}
       >
         {icon}
       </NavLink>
+      <span
+        className={
+          active
+            ? " font-semibold absolute  text-blue-500 whitespace-nowrap group-hover:opacity-100 duration-500 top-7"
+            : " font-semibold absolute opacity-0 text-blue-500 whitespace-nowrap group-hover:opacity-100 duration-500 top-7"
+        }
+      >
+        {name}
+      </span>
     </li>
   );
 };
