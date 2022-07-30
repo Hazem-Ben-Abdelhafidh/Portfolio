e317d93613edcc649f9aef2e3ca190bd2e2233df
Hazem-Ben-Abdelhafidh <morbidvisions@yahoo.com>
2022-07-30 15:52:34 +0100

    improving the projects page

--- src/Components/Porjects/Projects.jsx
+++ src/Components/Porjects/Projects.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Title from "../Title";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="flex flex-col h-screen w-full justify-center items-center">
+      <Title>Projects</Title>
+      <h1 className=" animate-ping text-3xl font-bold">Comming Soon...</h1>
+    </div>
+  );
 };
 
 export default Projects;
