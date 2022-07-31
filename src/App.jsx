5ce5d5708a059bf33975e1490fee722402531e3f
Hazem-Ben-Abdelhafidh <morbidvisions@yahoo.com>
2022-07-31 23:46:35 +0100

    adding google analytics 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import AppRoutes from "./Components/AppRoutes";
 import Header from "./Components/Header/Header";
-
+import ReactGA from "react-ga4";
 function App() {
+  ReactGA.initialize("G-9VXVDGCJ5B");
+  ReactGA.send("pageview");
+
   return (
     <div className=" text-gray-700">
       <Header />
